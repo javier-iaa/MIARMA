378e37389c4fb08b734e41aac8c47144af477b79
Javier PG <javier@iaa.es>
2020-05-21 13:58:28 +0200

    0.2 increased marks and error control

--- Plus/plotint.m
+++ Plus/plotint.m
@@ -5,30 +5,37 @@ function plotint(s1, s2, interp, yfor, yback)
 % varargin{1} is the forward extrapolation
 % varargin{2} is the backward extrapolation
 %
-% Version: 0.1
+% Version: 0.2
 % Changes from the last version:
+% - Bug fixes
+% - Marks increased.
 %
 %  Author(s): Javier Pascual-Granado
-%  Date: 19/03/2020
+%  Date: 20/05/2020
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     figure;
     ls1 = length(s1);
     ls2 = length(s2);
     np = length(interp);
+    
+    if isnan(interp)
+        fprintf('interp is nan');
+        return
+    end
 
     % Plot data segments
-    plot(1:ls1, s1, 'b.')
+    plot(1:ls1, s1, 'b.', 'MarkerSize', 18)
     hold
-    plot((ls1+np+1):(ls1+np+ls2), s2, 'm.')
+    plot((ls1+np+1):(ls1+np+ls2), s2, 'm.', 'MarkerSize', 18)
 
     % Plot the interpolation
-    plot((1+ls1):(ls1+np), interp, 'k.')
+    plot((1+ls1):(ls1+np), interp, 'k.', 'MarkerSize', 18)
 
     % Plot extrapolations
     if nargin > 3
-        plot(ls1:(ls1+np), yfor, 'r.')
-        plot((1+ls1):(1+ls1+np), yback, 'g.')
+        plot(ls1:(ls1+np), yfor, 'r.', 'MarkerSize', 18)
+        plot((1+ls1):(1+ls1+np), yback, 'g.', 'MarkerSize', 18)
     end
 
 end
