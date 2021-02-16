858ed331161f6e3a477002b635ee02fe4bf73f45
Javier PG <javier@iaa.es>
2021-02-16 10:33:56 +0100

    armaord.m version 1.1.0

--- armaord.m
+++ armaord.m
@@ -13,11 +13,18 @@ function varargout = armaord(S, varargin)
 %   varargout{2} = pmin
 %   varargout{3} = pmax
 %   varargout{4} = size of the segment evaluated
-
-% Version: 1.0.10
-% Changes: bug fixes.
-% Author: Javier Pascual-Granado
-% $Date: 21/05/2020$
+%
+% By Javier Pascual-Granado
+% <a href="matlab:web http://www.iaa.es;">IAA-CSIC, Spain</a>
+%
+% Version: 1.1.0
+%
+% Changes: 
+% - New parameter qmax
+% - Print number of models to be calculated.
+% - Multiple minor corrections and code optimization.
+%
+% Date: 12/02/2021
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Information Criterion
@@ -78,87 +85,171 @@ else
     pmax = varargin{ipM+1};
 end
 
+ipq = find(strcmp(varargin,'qmax'), 1);
+if isempty(ipq),
+    qmax = pmax;
+else
+    qmax = varargin{ipq+1};
+end
+
 seg = S;
 
 %% Akaike matrix
 r = pmax - pmin + 1;
+c = qmax + 1;
+nmod = (pmax-pmin+1)*(qmax+1);
 
-akamat = NaN( r, pmax+1 );
+akamat = NaN( r, c );
 
-% We use modo as a flag to indicate whether to write the results on a file
-% or not
-modo = false;
+fprintf(' p in [%d, %d] and q in [0, %d]\n', pmin, pmax, qmax);
+fprintf(' %d models to be calculated\n', nmod);
 
 iw = find( strcmp(varargin,'w'), 1 );
-if ~isempty(iw),
-    nom = varargin{iw+1};
-    nomfich = strcat( nom, ext );
+if ~isempty(iw),    
+    if length(varargin)>iw
+        idname = varargin{iw+1};
+    else
+        idname = 'temp';
+    end
+    pminstr = num2str( pmin, '%03.f' );
+    pmaxstr = num2str( pmax, '%03.f' );
+    qmaxstr = num2str( qmax, '%03.f' );
+    nomfich = [idname '_' pminstr '_' pmaxstr '_' qmaxstr ext];
     
-    % Look for aka temp files, get the p interval and load akamat
-    idname = nom(1:end-8);
+    % Look for aka files, get the parameters and load akamat
     d = dir([idname '*' ext]);
+    nd = length(d);
 %     d = dir(['temp_*_*' ext]);
-    pmax0 = zeros(size(d));
-    pmin0 = zeros(size(d));
+    pmax0 = zeros(1,nd);
+    pmin0 = zeros(1,nd);
+    qmax0 = zeros(1,nd);
+    
     if ~isempty(d),
         for k=1:length(d)
             lst_cells = regexp( d(k).name, '_', 'split');
-            pmin0(k) = str2num( lst_cells{end-1} );
-            cell_pmax= regexp( lst_cells{end}, '\.', 'split');
-            pmax0(k) = str2num( cell_pmax{1} );
+            pmin0(k) = str2num( lst_cells{end-2} );
+            pmax0(k) = str2num( lst_cells{end-1} );
+            cell_qmax= regexp( lst_cells{end}, '\.', 'split');
+            qmax0(k) = str2num( cell_qmax{1} );
         end
-        pmx0 = max(pmax0);
-        pmin0 = pmin0(pmax0==pmx0);
+        
+        % Select the file with highest pmax, then lowest pmin, the highest 
+        % qmax, i.e. the file with the broadest range. Note that this might 
+        % be not the best strategy in some cases so this might change in 
+        % future versions of the program.
+        pmx0 = max( pmax0 );
+        pmn0 = min( pmin0( pmax0==pmx0 ) );
+        qmx0 = max( qmax0( pmax0==pmx0 & pmin0==pmn0 ) );
         pmax0 = pmx0;
-%         lst_cells = regexp( d(end).name, '_', 'split');
-%         pmin0 = str2num( lst_cells{2} );
-%         cell_pmax = lst_cells{3};
-%         pmax0 = str2num( cell_pmax(1:end-4) );
-%         akamat0 = dlmread( d(end).name );
-        pmin0str = num2str(pmin0,'%03.f');
-        pmax0str = num2str(pmax0,'%03.f');
-        akaname = [idname '_' pmin0str '_' pmax0str ext];
+        pmin0 = pmn0;
+        qmax0 = qmx0;
+        
+        pmin0str = num2str( pmin0, '%03.f' );
+        pmax0str = num2str( pmax0, '%03.f' );
+        qmax0str = num2str( qmax0, '%03.f' );
+        akaname = [ idname '_' pmin0str '_' pmax0str '_' qmax0str ext ];
+        
+        fprintf(' Found Akaike file %s\n', akaname);
         akamat0 = dlmread( akaname );
         akamat0 = akamat0';
-        [a, b] = size( akamat0 );
-    
-        if (pmin >= pmin0)
-            if (pmin > pmax0)
+              
+        if ( pmin >= pmin0 )
+            if ( pmin > pmax0 )
+                % Case ix) 
                 % Disjoint intervals. Not implemented yet
                 err = MException('ResultChk:OutOfRange', ...
                     'Outside of expected range, not implemented yet');
                 throw(err)
-            elseif (pmax <= pmax0)
-                akamat = akamat0(1:r,1:(pmax+1));
-                varargout{1} = akamat;
-                return
+            elseif ( pmax <= pmax0 )
+                if ( qmax <= qmax0 )
+                    % Case iv) 
+                    ii0 = pmin - pmin0 + 1;
+                    ie0 = pmax - pmin0 + 1;
+                    je0 = qmax + 1;
+                    akamat = akamat0( ii0:ie0, 1:je0 );
+                    varargout{1} = akamat;
+                    fprintf(' All coeffs. already calculated\n');
+                    return
+                else
+                    % Case iii)
+                    % Coeffs. in the subset pmax x je are read from akamat0
+                    ii0 = pmin - 1;
+                    ie0 = pmax - 1;
+                    je = qmax0 + 1;
+                    akamat(:,1:je ) = akamat0( ii0:ie0, :);
+                    nsav = pmax*je;
+                end
             else
-                % Note that the final Akaike matrix collect the maximum
-                % number of coefficients so in this case it will be a
-                % pmin0 x pmax matrix
-                pmaxstr = num2str(pmax,'%03.f');
-                nomfich = [idname '_' pmin0str '_' pmaxstr ext];
-                r = pmax - pmin0 + 1;
-                akamat = NaN( r, pmax+1 );
-                akamat(1:a,1:b) = akamat0;
+                if (qmax<=qmax0)
+                    % Case ii)
+                    % Coeffs. in the subset ie x qmax are read from akamat0
+                    ie = pmax0 - pmin + 1;
+                    ii0 = pmin - 1;
+                    je0 = qmax + 1;
+                    akamat(1:ie ,:) = akamat0( ii0:end, 1:je0);
+                    nsav = ie*qmax;
+                else
+                    % Case i)
+                    % Coeffs. in subset ie x je are read from akamat0
+                    ie = pmax0 - pmin;
+                    ii0 = pmin - 1;
+                    je = qmax0 + 1;
+                    akamat(1:ie ,1:je) = akamat0( ii0:end, :);
+                    nsav = ie*je;
+                end
             end
         else
             if (pmax < pmin0)
+                % Case x)
                 % Disjoint intervals. Not implemented yet
                 err = MException('ResultChk:OutOfRange', ...
                     'Outside of expected range, not implemented yet');
                 throw(err)
-            elseif (pmax <= pmax0)
-                pminstr = num2str(pmin,'%03.f');
-                nomfich = [idname '_' pminstr '_' pmax0str ext];
-                r = pmax0 - pmin + 1;
-                akamat = NaN( r, pmax0+1 );
+            elseif ( pmax < pmax0 )
+                if ( qmax <= qmax0 )
+                    % Case viii)
+                    % Coeffs. in the subset (pmax -ii) x qmax are read from 
+                    % akamat0
+                    ii = pmin0 - pmin + 1;
+                    ie0 = pmax - pmin0 + 1;
+                    je0 = qmax + 1;
+                    akamat( ii:end, : ) = akamat0( 1:ie0, 1:je0);
+                    nsav = (pmax - ii)*qmax;
+                else
+                    % Case vii)
+                    % Coeffs. in the subset (pmax -ii) x je are read from 
+                    % akamat0
+                    ii = pmin0 - pmin + 1;
+                    ie0 = pmax - pmin0 + 1;
+                    je = qmax0 + 1;
+                    akamat( ii:end, 1:je ) = akamat0( 1:ie0, :);
+                    nsav = (pmax - ii)*je;
+                end
+            else
+                if (qmax<=qmax0)
+                    % Case vi)
+                    % Coeffs. in the subset (ie-ii+1) x qmax are read from 
+                    % akamat0
+                     ii = pmin0 - pmin + 1;
+                     ie = pmax0 - pmin + 1;
+                     je0 = qmax + 1;
+                     akamat(ii:ie ,:) = akamat0( :, 1:je0);
+                     nsav = (ie - ii + 1)*qmax;
+                else
+                    % Case v)
+                    % Coeffs. in subset (ie-ii+1) x je are read from akamat0
+                    ii = pmin0 - pmin + 1;
+                    ie = pmax0 - pmin + 1;
+                    je = qmax0 + 1;
+                    akamat(ii:ie ,1:je) = akamat0;
+                    nsav = (ie - ii + 1)*je;
+                end
             end
-            % if pmax > pmax0 nothing needs to be done, nomfich and akamat 
-            % are preserved
         end
     end
-    modo = true;
+    
+    ncal = nmod - nsav;
+    fprintf(' %d models loaded, %d need to be calculated.\n', nsav, ncal);
     fichw = fopen(nomfich, 'w');
 end
 
@@ -166,48 +257,35 @@ end
 % This is the main loop where the Akaike coefficients are calculated for
 % each pair (p,q)
 
-% h = waitbar(0,sprintf(...
-% 'Step 2 - Order estimation\n     Please wait...'));
+tt0 = cputime;
+tt = tt0;
 
-for i=0:pmax,
-    
-%     waitbar((i+1)/(1+pmax),h)
-    fprintf('%.f%%... ', 100*(i+1)/(1+pmax));
+for i=0:qmax,
     
     akam = akamat(:,1+i);
     
 %     ir = i*r;
     
-%     fprintf('Step %d\n',1+i);
-        for j=pmin:pmax,
-            ii = j-pmin+1;
+        for j=pmin:pmax
+            ii = j - pmin + 1;
 %             jj = 1+i;
 %             ind = ir + ii;
 %             aka = akamat(ii,jj);
             aka = akam(ii);
 %             aka = akamat(ind);
-            if ~isnan(aka),
-                if modo,
-                    fprintf(fichw,'%f ',aka);
+            if ~isnan(aka)
+                if ~isempty(iw),
+                    fprintf( fichw, '%f ', aka);
                 end
-%                 fprintf(repmat('\b',1,5));
                 continue;
             end
             
-%             d = i+j;     
-
-            % Special four-stage LS-IV iterative algorithm
-%             seg1 = seg;
             try               
-%                 model = armax(double(single(seg)),[j i],'Focus', ...
-%                     'stability','SearchMethod','lm','MaxIter',nit, ...
-%                     'LimitError',1);
-%                 model = armax(seg, [j i]);
                 model = armax(seg, [j i], 'SearchMethod', 'lsqnonlin');
             catch E
                 % Report the error messages to a file
                 if exist('nomfich','var'),
-                    errfich = strcat(nom,'_EM.htm');
+                    errfich = strcat( [ nomfich(1:end-4) '_EM.htm' ] );
                     fich = fopen(errfich,'a');
                 else
                     fich = fopen('Error_Messages.htm','a');
@@ -217,34 +295,37 @@ for i=0:pmax,
                 fprintf(fich,'%s\n',msg);
                 fclose(fich);
                 
-                if modo,
+                if ~isempty(iw)
                     fprintf(fichw,'NaN ');
                 end
-%                 fprintf(repmat('\b',1,5));
                 continue;
             end
             
-            aka = aicplus(model,IC);
+            aka = aicplus(model, IC);
             
             akam(ii) = aka;
 %             akamat(ii,jj) = aka;
             
-            if modo,
-                fprintf(fichw,'%f ',aka);
+            if ~isempty(iw)
+                fprintf( fichw, '%f ', aka);
             end 
         end
         
-        akamat(:,1+i) = akam;
+        akamat(:, 1+i) = akam;
         
-        if modo,
-            fprintf(fichw,'\n');
+        if ~isempty(iw)
+            fprintf( fichw, '\n');
         end
-%         fprintf(repmat('\b',1,5));
+        
+        per = 100*(i+1)/(1+qmax);
+        now = cputime;
+        ee = now - tt;
+        fprintf('%.f%%...   %.f s last iteration, %.f since start\n', ...
+            per, ee, now-tt0); 
+        tt = cputime;
 end
 
-% close(h);
-
-if modo,
+if ~isempty(iw),
     fclose(fichw);
 end
 
@@ -271,4 +352,10 @@ elseif nargout==4,
     varargout{2} = pmin;
     varargout{3} = pmax;
     varargout{4} = L;
+elseif nargout==5,
+    varargout{1} = akamat;
+    varargout{2} = pmin;
+    varargout{3} = pmax;
+    varargout{4} = L;
+    varargout{5} = qmax;
 end
