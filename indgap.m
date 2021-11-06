157815b939a44162875eb91c8963b5359ef92c43
Javier PG <javier@iaa.es>
2021-11-06 02:39:00 +0100

    indgap 1.0.5

--- indgap.m
+++ indgap.m
@@ -1,12 +1,12 @@
-function ind1 = indgap(flag,varargin)
-% function ind1 = indgap(flag,varargin) find the indexes corresponding of
+function ind1 = indgap(flag)
+% function ind1 = indgap(flag) find the indexes corresponding of
 % the start and end of each gap based on the flag assigned to the
 % datapoints.
 
-% Changes from the last version: gaps can be -1 or +1
-% Version: 1.0.4
+% Changes from the last version: minor changes
+% Version: 1.0.5
 % Author(s): Javier Pascual-Granado
-% Date: 01/10/2019
+% Date: 28/04/2021
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -18,18 +18,13 @@ end
 L = length(flag);
 ind1 = nan(1,L);
 
-% if nargin==1,
-%     h = waitbar(0,'Step 0 - Finding indexes...');
-% else
-%     h = waitbar(0,sprintf('Step %d - Rebuilding indexes...',varargin{1}));
-% end
-
 % Main loop. Gap start and end indexes are saved in ind1
-i=1;j=1;
+i=1;
+j=1;
 
 while i <= L,
     % First index inside the gap
-    ind = find(abs(flag(i:end))==1,1);
+    ind = find( abs( flag(i:end) )==1, 1);
     if ~isempty(ind),
         ind1(j) = i+ind-1;
         i = i+ind-1;
@@ -50,15 +45,13 @@ while i <= L,
         end
         break;
     end
-%     waitbar(i/L,h);
+
 end
 
 if ~exist('ind1','var'),
     ind1=[];
     return;
 end
-
-% close(h);
 
 % This sentence avoids some issues
 ind1(isnan(ind1))=[];
