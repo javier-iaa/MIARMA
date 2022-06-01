42c46f99e33d31c96652b17b3678adb9953a11be
Javier PG <javier@iaa.es>
2022-06-01 14:48:57 +0200

    v0.2

--- autoarmaord.m
+++ autoarmaord.m
@@ -27,28 +27,33 @@ function aka = autoarmaord( seg, varargin)
 % aka = autoarmaord( seg, 'verbose', false, 'w', akaname);
 % Akaike matrix is saved in file with name <akaname>.
 % Verbosity is set off.
-% 
+%
+% 'rep' followed by an integer sets the limit in the repetition of orders, 
+% otherwise repetition is not used as a stop criterion.
+%
 % By Javier Pascual-Granado
 % <a href="matlab:web http://www.iaa.es;">IAA-CSIC, Spain</a>
 %
-% Version: 0.1
+% Version: 0.2
 %
 % Changes:
-% - Introduced stop condition when the optimal order repeats several times
-% - Minor fixes.
+% - Added parameter rep_lim that controls limit in the repetition.
+% - Several crash tests. 
+% - Added parameter mseg to trim the segment.
+% - The search is repeated if the tests are not fulfilled for the trimmed segment
+% and the max length segment is used in that case.
 %
 % Calls:
-%  - armaord.m
-%  - validate_arma.m
 %
-% Date: 22/03/2022
+% Date: 27/04/2022
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Check inputs
-
-optargin = size(varargin, 2);
 verbflag = true;
 temp = true;
+ML = length(seg);
+
+% Check inputs
+optargin = size(varargin, 2);
 
 if optargin==0
     temp = false;
@@ -56,37 +61,42 @@ elseif optargin==1
     if ~strcmp(varargin{1},'w')
         error('Wrong inputs');
     end
-elseif optargin==2
-    if strcmp(varargin{1},'w')
-        if strcmp(varargin{2},'verbose')
-            error('Wrong inputs');
-        else
-            akaname = varargin{2};
+else
+    iw = find(strcmp(varargin, 'w'), 1);
+    if ~isempty(iw)
+        if ischar(varargin{iw+1})
+            akaname = varargin{iw+1};
         end
-    elseif strcmp(varargin{2},'w')
+    end
+    
+    iv = find(strcmp(varargin, 'verbose'), 1);
+    if ~isempty(iv)
+        if islogical(varargin{iv+1})
+            verbflag = varargin{iv+1};
+        else
             error('Wrong inputs');
-    elseif strcmp(varargin{1}, 'verbose')
-        verbflag = varargin{2};
+        end
     end
-elseif optargin==3
-    if ~strcmp(varargin{end},'w')
-        error('Wrong inputs');
-    elseif strcmp(varargin{1},'verbose')
-        verbflag = varargin{2};
+    
+    ir = find(strcmp(varargin, 'rep'), 1);
+    if ~isempty(ir)
+        rep_lim = varargin{ir+1};
     else
-        error('Wrong inputs');
+        rep_lim = 10;    % default value for repetition 
     end
-elseif optargin==4
-    if ~strcmp(varargin{3},'w')
-        error('Wrong inputs');
-    elseif strcmp(varargin{1}, 'verbose')
-        akaname = varargin{4};
-        verbflag = varargin{2};
-    else
-        error('Wrong inputs');
+    
+    im = find(strcmp(varargin, 'mseg'), 1);
+    if ~isempty(im)
+        % mseg is the max. length of the segment use to calculate the order
+        mseg = varargin{im+1};
+        
+        % If the statistical tests are not passed Akaike matrix will be recalculated 
+        % using seg0 instead of seg
+        seg0 = seg;
+        if ML>mseg
+            seg = seg(1:mseg);
+        end
     end
-elseif optargin>4
-    error('Wrong number of inputs');
 end
 
 % Initial conditions
@@ -102,77 +112,95 @@ qmax_lim = dqmax*10; % optional inputs too in next update
 p0 = 0;
 q0 = 0;
 cont = 0;
+lseg = length(seg);
 
-while ~isval
-    
-    % Calculate Akaike matrix
-    if exist( 'akaname', 'var' )
-        if verbflag
-            aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
-                'qmax', qmax, 'w', akaname);
-        else
-            aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
-            'qmax', qmax, 'verbose', false, 'w', akaname);
-        end
-        
-    elseif temp
-        if verbflag
-            aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
-                'qmax', qmax, 'w' );
-        else
-            aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
-            'qmax', qmax, 'verbose', false, 'w' );
-        end
-    else
-        if verbflag
-            aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
-                'qmax', qmax);
-        else
-            aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
-            'qmax', qmax, 'verbose', false);
-        end
-    end
-    
-    % Find optimal order
-    [cp, cq] = find( aka == min( min( aka ) ) );
-    q = cq - 1;
-    p = cp + pmin - 1;
-    ord = [p q];
-    fprintf('\nOptimal order is (%d, %d)\n', p, q);
-    
-    % Stop condition: if the same order is found in more than 2 iterations
-    if p==p0 && q==q0
-        cont = cont + 1;
-        if cont==2
-            return
-        end
-    else
-        cont = 0;
-    end
-    p0 = p;
-    q0 = q;
+while lseg<=ML
 
-    % Validate model with optimal order
-    [isval_mse, sta_mse] = validate_arma( seg, ord, facint, 'mse' );
-    [isval_pc, sta_pc] = validate_arma( seg, ord, facint, 'pc' );
-    if isval_mse
-        str_mse = 'cannot reject';
-    else
-        str_mse = 'reject';
+    while ~isval
+
+        % Calculate Akaike matrix
+        if exist( 'akaname', 'var' )
+            if verbflag
+                aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
+                    'qmax', qmax, 'w', akaname);
+            else
+                aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
+                'qmax', qmax, 'verbose', false, 'w', akaname);
+            end
+
+        elseif temp
+            if verbflag
+                aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
+                    'qmax', qmax, 'w' );
+            else
+                aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
+                'qmax', qmax, 'verbose', false, 'w' );
+            end
+        else
+            if verbflag
+                aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
+                    'qmax', qmax);
+            else
+                aka = armaord( seg, 'pmin', pmin, 'pmax', pmax, ...
+                'qmax', qmax, 'verbose', false);
+            end
+        end
+
+        % Find optimal order
+        [cp, cq] = find( aka == min( min( aka ) ) );
+        q = cq - 1;
+        p = cp + pmin - 1;
+        ord = [p q];
+        fprintf('\nOptimal order is (%d, %d)\n', p, q);
+
+        % Stop condition: if the same order is found in more than rep_lim iterations
+        if exist('rep_lim', 'var')
+            if p==p0 && q==q0
+                cont = cont + 1;
+                if cont==rep_lim
+                    break
+                end
+            else
+                cont = 0;
+            end
+            p0 = p;
+            q0 = q;
+        end
+
+        % Validate model with optimal order
+        [isval_mse, sta_mse] = validate_arma( seg, ord, facint, 'mse' );
+        [isval_pc, sta_pc] = validate_arma( seg, ord, facint, 'pc' );
+        if isval_mse
+            str_mse = 'cannot reject';
+        else
+            str_mse = 'reject';
+        end
+        if isval_pc
+            str_pc = 'cannot reject';
+        else
+            str_pc = 'reject';
+        end
+        fprintf('\nMSE test %s null hypothesis with h=%4.1e\n', str_mse, sta_mse);
+        fprintf('PC test %s null hypothesis with h=%4.1e\n', str_pc, sta_pc);
+        isval = isval_mse && isval_pc;
+
+        pmax = pmax + dpmax;
+        qmax = qmax + dqmax;
+
+        if pmax>pmax_lim || qmax>qmax_lim
+            break
+        end
+
     end
-    if isval_pc
-        str_pc = 'cannot reject';
-    else
-        str_pc = 'reject';
+    
+    if isval
+        return
     end
-    fprintf('\nMSE test %s null hypothesis with h=%4.1e\n', str_mse, sta_mse);
-    fprintf('PC test %s null hypothesis with h=%4.1e\n', str_pc, sta_pc);
-    isval = isval_mse && isval_pc;
     
-    pmax = pmax + dpmax;
-    qmax = qmax + dqmax;
-    
-    if pmax>pmax_lim || qmax>qmax_lim
+    if lseg<ML
+        seg = seg0;
+        lseg = ML;
+    else
         return
     end
     
