6d467b8de0cd84196d63dade3f87f79e79570877
javier-iaa <javier@iaa.es>
2023-08-17 00:58:09 +0200

    v0.3 using median

--- Plus/sampest.m
+++ Plus/sampest.m
@@ -2,37 +2,30 @@ function samp = sampest(timein)
 % function samp = sampest(timein) estimates the sampling of the time series
 % with times in timein.
 %
-%  Version: 0.2
+%  Version: 0.3
 %  Changes: histogram of the sampling intervals. This will be deprecated in
 %  future versions using the Bayesian Blocks algorithm to calculate the
 %  histogram since the classical histogram gives a poor resolution and it
 %  is necessary a huge number of bins.
 %
-% Changes: fix a bug in Ln 25
+% Changes: changed mean into median and simplified.
 %
 %  Author: Javier Pascual-Granado
-%  $Date: 18/11/2021 $
+%  $Date: 12/04/2023 $
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 L = length(timein);
 
 % Time interval between measures
-dt = timein(2:L)-timein(1:L-1);
+dt = diff(timein);
 
 %% Estimate sampling
-% a 1-percent deviation is admitted
 [n, dtm] = hist(dt, L-1);
 [~, I] = max(n);
 samp0 = dtm(I);
-% samp0 = timein(2)-timein(1);
-err0 = samp0/2; 
-% ijump = find(abs(dt-samp0)>err0,1);
 
-% the estimate is refined
-% samp1 = mean(dt(1:(ijump-1)));
-% err1 = samp1/100;
+err0 = samp0/2; 
 
 % final estimate is obtained
-samp = mean( dt( abs(dt-samp0)<err0 ) );
-% err = samp/100;                                                 
+samp = median( dt( abs(dt-samp0)<err0 ) );
